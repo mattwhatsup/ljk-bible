dcbdad373016e3355f1a1951c8570cdbf6abc826
matt zhou <matt@cruelyouth.com>
2023-03-13 06:46:19 +0800

    rename build files & fix opf

---
+++ rename.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mv ./build/bible.cn-nc.epub   ./build/ljk-sim.epub
+mv ./build/bible.cn.epub      ./build/ljk-simc.epub
+mv ./build/bible.tw-nc.epub   ./build/ljk-tr.epub
+mv ./build/bible.tw.epub      ./build/ljk-trc.epub
